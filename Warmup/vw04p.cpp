cfb1290885b4e37834e952f89ba8c3a7e7108a68
TumoniacXT <46486571+BenTumoniacXT@users.noreply.github.com>
2021-05-08 18:04:41 +0700

    Update 08/05/2021: Add VW04p, Update Notes

--- Warmup/vw04p.cpp
+++ Warmup/vw04p.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
+
+long double d, a1, a2, v, t;
+
+long double calc()
+{
+    long double rs;
+
+    long double a = (long double)1 / (a1 * 2) + (long double)1 / (a2 * 2), v2 = (-t + sqrt(t * t + 4 * a * d)) / (a * 2);
+    if (v2 <= v) rs = v2 / a1 + v2 / a2 + t;
+        else rs = d / v + v / (2 * a1) + v / (2 * a2);
+
+    return rs + 5e-14;
+}
 
 int main()
 {
-    //Code is under Construction
+    cin >> d >> a1 >> a2 >> v >> t;
+    cout << setprecision(12) << fixed << calc();
 
     return 0;
 }
